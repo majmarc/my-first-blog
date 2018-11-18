1f6edd39426227b53c11e1be5f7026afddb8ace2
marcin <maironkyo@o2.pl>
2018-11-18 17:20:05 +0100

    update post list html

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
+from datetime import timezone
 from django.shortcuts import render
+from .models import Post
 
 
 def	post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
